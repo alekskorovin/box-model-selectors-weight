96f4294036cd682d63902820fa9e42d1ea9bd0d1
Aleksey Korovin <alekskorovin@gmail.com>
2016-09-19 09:51:44 +0300

    added several sections

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+document.querySelectorAll('.box-model').forEach(function(element) {
+    element.addEventListener('mouseover', function(event) {
+        element.classList.add('hover');
+        event.stopPropagation();
+
+    });
+    element.addEventListener('mouseout', function(event) {
+        element.classList.remove('hover');
+        event.stopPropagation();
+    });
+});
